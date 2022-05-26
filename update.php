b257c25aba547dbdfcb286a5335fa72435413904
felipeverse <felipealvesrrodrigues@outlook.com>
2022-05-26 12:01:05 -0400

    fix some bugs

--- update.php
+++ update.php
@@ -4,10 +4,12 @@
 const modules_dir = "./modules";
 
 $modules = get_modules(modules_dir);
-var_dump($modules);
+if (empty($modules))
+    echo "empty modules directory." . "\n";
 
 foreach ($modules as $module) {
     $module->manuals = get_manuals($module->path);
+    echo "module imported: " . $module->name . "\n";
 }
 
 file_put_contents(__DIR__ . "/data.json", json_encode($modules, JSON_PRETTY_PRINT));
@@ -19,7 +21,7 @@ function get_modules($dir)
     $files_in_dir = scandir($dir);
 
     foreach ($files_in_dir as $file) {
-        if ($file != '.' && $file != '..')
+        if ($file != '.' && $file != '..' && (!is_file($dir . "/" . $file)))
         {
             $module = new stdClass;
             $module->name = $file;
